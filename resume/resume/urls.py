0a1e0bd3d003c3f2d7f783cb5dc8ae076bb1df81
aviv.dodi <aviv.dodi@scrdairy.com>
2022-06-05 20:27:47 +0300

    views, index.html, runserver

--- resume/resume/urls.py
+++ resume/resume/urls.py
@@ -17,10 +17,12 @@ from django.contrib import admin
 from django.urls import path
 from django.conf.urls.static import static
 from django.conf import settings
+from project import views
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.index, name='index')
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
