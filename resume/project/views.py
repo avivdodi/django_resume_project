0672ea0fcbfbf9ac75f92ec96959fedf19839adf
aviv.dodi <aviv.dodi@scrdairy.com>
2022-06-05 21:37:10 +0300

    projects detail

--- resume/project/views.py
+++ resume/project/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Project
+
 
 # Create your views here.
 
@@ -10,3 +11,11 @@ def index(request):
         'projects': projects
     }
     return render(request, 'project/index.html', context)
+
+
+def detail(request, project_id):
+    project = get_object_or_404(Project, id=project_id)
+    context = {
+        'project': project
+    }
+    return render(request, 'project/detail.html', context)
