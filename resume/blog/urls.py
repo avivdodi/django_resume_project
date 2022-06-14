992836bd3b2f686a21193cb911afbbf3f66ad0e2
aviv.dodi <aviv.dodi@scrdairy.com>
2022-06-14 17:23:36 +0300

    blog html, model, view

--- resume/blog/urls.py
+++ resume/blog/urls.py
@@ -5,5 +5,6 @@ from . import views
 app_name = 'blog'
 
 urlpatterns = [
-    path('', views.all_blogs, name='all_blogs')
+    path('', views.all_blogs, name='all_blogs'),
+    path('<int:blog_id>', views.blog_detail, name='blog_detail')
 ]
